3c560671bc2babcf1e78617b94f32d0928c7a92a
kizya007 <kizya007@mail.ru>
2023-05-25 22:19:08 +0200

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import "../assets/Header.scss"
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="navigation">
+        <a href="" className="navigation-link">Home1</a>
+        <a href="" className="navigation-link">Menu</a>
+        <a href="" className="navigation-link">About us</a>
+        <a href="" className="navigation-link">Our Story</a>
+        <a href="" className="navigation-link">Blog</a>
+        <a href="" className="navigation-link">Contact</a>
+        <a href="" className="navigation-link">Clone Project</a>
+      </nav>
+    </header>
+  );
+}
+export default Header;
